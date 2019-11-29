701c1658f03d50190b4d544147542d422636beef
choiyeonwoo <rud527@naver.com>
2019-11-29 22:30:43 +0900

    version12
    CustomerService is updated

--- src/com/beagle/java/projects/starfucks/service/CustomerService.java
+++ src/com/beagle/java/projects/starfucks/service/CustomerService.java
@@ -2,38 +2,28 @@ package com.beagle.java.projects.starfucks.service;
 
 import com.beagle.java.projects.starfucks.StarFucksList;
 import com.beagle.java.projects.starfucks.domain.Customer;
-import com.beagle.java.projects.starfucks.repository.CustomerRepository;
+
+
 
 public class CustomerService {
-    
-    // Variable that temporarily stores data in StarFucksList form while the program is running.
-    CustomerRepository customerRepository = new CustomerRepository();
-    private StarFucksList<Customer> temporaryStorage = customerRepository.readAllCustomer();
 
-    public StarFucksList<Customer> getTemporaryStorage() {
-        return this.temporaryStorage;
-    }
 
-    private void setTemporaryStorage(StarFucksList<Customer> temporaryStorage) {
-        this.temporaryStorage = temporaryStorage;
-    }
-
-    public void insertCustomerData(Customer inputClass) {
-        StarFucksList<Customer> newList = getTemporaryStorage();
+    public StarFucksList<Customer> insertCustomerData(StarFucksList<Customer> temporaryStorage, Customer inputClass) {
+        StarFucksList<Customer> newList = temporaryStorage;
         newList.addLast(inputClass);
-        setTemporaryStorage(newList);
+        return newList;
     }
 
 
-    public void updateCustomerData(String id, Customer newData) {
-        StarFucksList<Customer> newList = getTemporaryStorage();
+    public StarFucksList<Customer> updateCustomerData(StarFucksList<Customer> temporaryStorage, String id, Customer newData) {
+        StarFucksList<Customer> newList = temporaryStorage;
         for (int i = 0; i < newList.size(); i++) {
             if (newList.get(i).getId().equals(id)) {
                 newList.add(i, newData);
                 newList.remove(i+1);
             }
         }
-        setTemporaryStorage(newList);
+        return newList;
     }
 
 
